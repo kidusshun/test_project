7edd77d5b9cc1ba59424bb72a37f4e244ff51d2e
kidusshun <kidusshun@gmail.com>
2024-03-19 11:18:50 +0300

    add workflows

--- product.py
+++ product.py
@@ -2,14 +2,14 @@ import unittest
 
 class Product:
     def __init__(self, name, price, quantity) -> None:
-        if price < 0 or quantity < 0:
-            raise ValueError('Price and quantity must be greater than 0')
         
         self.name = name
         self.price = price
         self.quantity = quantity
 
     def calculateTotal(self):
+        if self.quantity < 0 or self.price < 0:
+            raise ValueError('Price and quantity must be greater than 0')
         return self.price * self.quantity
     
 
